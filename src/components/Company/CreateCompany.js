3c96363b63002c5bcc2b2532e3d49b2573e14241
MarioGeorgiev <37220117+MarioGeorgiev@users.noreply.github.com>
2023-03-28 20:34:19 +0300

    Form validation for company creation

--- src/components/Company/CreateCompany.js
+++ src/components/Company/CreateCompany.js
@@ -11,7 +11,17 @@ export default function CreateCompany({
     VATNumber: "",
     logoUrl: "",
     phone: "",
-    email: ""
+    email: "",
+    
+  })
+  const [errorsValidation, setErrorsValidation] = useState({
+    companyName: false,
+    address: false,
+    manager:false,
+    VATNumber: false,
+    logoUrl: false,
+    phone: false,
+    email:false
   })
   const onChangeHandler = (e) => {
     setValues(state => ({ ...state, [e.target.name]: e.target.value }))
@@ -20,6 +30,35 @@ export default function CreateCompany({
     e.preventDefault()
     onCreateCompany(values)
     setAddCompany(false)
+  }
+  const onFormValidationHandler = (e) => {
+    
+    if (e.target.name === "companyName" && e.target.value.length < 2) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))      
+
+    }else if (e.target.name === "address" && (e.target.value.length === 0 || e.target.value.trim() === '')) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    } 
+    else if (e.target.name === "manager" && (e.target.value.length === 0 || e.target.value.trim() === '')) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    } 
+    else if (e.target.name === "VATNumber" && (e.target.value.length === 0 || e.target.value.trim() === '')) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    }
+    else if (e.target.name === "logoUrl" && (!e.target.value.startsWith("http")|| !e.target.value.startsWith("https"))) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    }
+    else if (e.target.name === "email" && !e.target.value.includes("@")) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    }
+    else if (e.target.name === "phone" && (isNaN(e.target.value) || e.target.value.length!==10)) {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: true }))
+    }
+    else {
+      setErrorsValidation(state => ({ ...state, [e.target.name]: false }))
+      
+    }
+    ///console.log(errorsValidation)
   }
   return (
     <div className={styles["overlay"]}>
@@ -38,21 +77,28 @@ export default function CreateCompany({
                 <label htmlFor="companyName">Company name</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="firstName" name="companyName" type="text" value={values.companyName} onChange={(e) => onChangeHandler(e)} />
+                  <input id="firstName" name="companyName" type="text"
+                    value={values.companyName}
+                    onChange={(e) => onChangeHandler(e)}
+                    onBlur={(e) => onFormValidationHandler(e)} />
                 </div>
-                <p className={styles["form-error"]}>
-                  Company should be at least 3 characters long!
-                </p>
+                
+                {errorsValidation.companyName === true &&
+                  <p className={styles["form-error"]}>Company should be at least 2 characters long!</p>
+                }
               </div>
               <div className={styles["form-group"]}>
                 <label htmlFor="address">Address</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="lastName" name="address" type="text" value={values.address} onChange={(e) => onChangeHandler(e)} />
+                  <input id="lastName" name="address" type="text" value={values.address} 
+                  onChange={(e) => onChangeHandler(e)}
+                  onBlur={(e) => onFormValidationHandler(e)}/>
                 </div>
+                {errorsValidation.address === true &&
                 <p className={styles["form-error"]}>
                   Address is required!
-                </p>
+                </p>}
               </div>
             </div>
             <div className={styles["form-row"]}>
@@ -60,49 +106,63 @@ export default function CreateCompany({
                 <label htmlFor="manager">Manager</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="email" name="manager" type="text" value={values.manager} onChange={(e) => onChangeHandler(e)} />
-                </div>
-                <p className={styles["form-error"]}>Email is not valid!</p>
+                  <input id="manager" name="manager" type="text" value={values.manager} 
+                  onChange={(e) => onChangeHandler(e)}
+                  onBlur={(e) => onFormValidationHandler(e)} />
+                </div>{errorsValidation.manager === true &&
+                <p className={styles["form-error"]}>Manager is required</p>}
               </div>
               <div className={styles["form-group"]}>
                 <label htmlFor="VATNumber">VAT number</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="phoneNumber" name="VATNumber" type="text" value={values.VATNumber} onChange={(e) => onChangeHandler(e)} />
-                </div>
+                  <input id="VATNumber" name="VATNumber" type="text" value={values.VATNumber} 
+                  onChange={(e) => onChangeHandler(e)}
+                  onBlur={(e) => onFormValidationHandler(e)}
+                   />
+                </div>{errorsValidation.VATNumber &&
                 <p className={styles["form-error"]}>
-                  Phone number is not valid!
-                </p>
+                  VAT number is not valid!
+                </p>}
               </div>
             </div>
             <div className={styles["form-group long-line"]}>
               <label htmlFor="logoUrl">Log Url</label>
               <div className={styles["input-wrapper"]}>
 
-                <input id="imageUrl" name="logoUrl" type="text" value={values.logoUrl} onChange={(e) => onChangeHandler(e)} />
+                <input id="logoUrl" name="logoUrl" type="text" value={values.logoUrl} 
+                onChange={(e) => onChangeHandler(e)}
+                onBlur={(e) => onFormValidationHandler(e)} />
               </div>
-              <p className={styles["form-error"]}>Logo is not valid!</p>
+              {errorsValidation.logoUrl &&
+              <p className={styles["form-error"]}>Logo should start with HTTP/HTTPS</p>}
             </div>
             <div className={styles["form-row"]}>
               <div className={styles["form-group"]}>
                 <label htmlFor="phone">Phone</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="country" name="phone" type="text" value={values.phone} onChange={(e) => onChangeHandler(e)} />
+                  <input id="phone" name="phone" type="text" value={values.phone}
+                   onChange={(e) => onChangeHandler(e)}
+                   onBlur={(e) => onFormValidationHandler(e)} />
                 </div>
+                {errorsValidation.phone &&
                 <p className={styles["form-error"]}>
                   Phone should have 10 digits!
-                </p>
+                </p>}
               </div>
               <div className={styles["form-group"]}>
                 <label htmlFor="email">Email</label>
                 <div className={styles["input-wrapper"]}>
 
-                  <input id="city" name="email" type="text" value={values.email} onChange={(e) => onChangeHandler(e)} />
+                  <input id="email" name="email" type="text" value={values.email}
+                   onChange={(e) => onChangeHandler(e)}
+                   onBlur={(e) => onFormValidationHandler(e)} />
                 </div>
+                {errorsValidation.email &&
                 <p className={styles["form-error"]}>
                   Email is required!
-                </p>
+                </p>}
               </div>
             </div>
             {/*<div className={styles["form-row"]}>
@@ -118,7 +178,7 @@ export default function CreateCompany({
               </div>
 
               </div>*/}
-              
+
             <div id={styles["form-actions"]}>
               <button id={styles["action-save"]} className={styles["btn"]} type="submit">Save</button>
               <button id={styles["action-cancel"]} className={styles["btn"]} type="button" onClick={() => setAddCompany(false)}>
